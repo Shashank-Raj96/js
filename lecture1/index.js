2c42bd592608aa3d244a192d0781d927bea01aef
Shashank-Raj96 <shashankraj0915@gmail.com>
2024-11-27 23:48:04 +0530

    first commit

---
+++ lecture1/index.js
@@ -0,0 +1,7 @@
+var a=2;
+function b(num){
+    var ans=num*num;
+    return ans;
+}
+console.log(b(a))
+var e=b(4)
---
+++ lecture2/index.js
@@ -0,0 +1,15 @@
+
+
+
+var x=7;
+function getname(){
+    var a=10
+    console.log("Namaste javascript");
+}
+getname();
+console.log(x);
+console.log(getname);
+  
+
+
+
