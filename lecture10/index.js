66cf66d2844b6162a24324ec0c4d1e87745daa80
Shashank-Raj96 <shashankraj0915@gmail.com>
2024-12-07 17:04:23 +0530

    setTimeout using var 2

--- lecture10/index.js
+++ lecture10/index.js
@@ -1,11 +1,15 @@
 function x(){
    
-    for( var i = 1; i<= 5; i++)
-    setTimeout (function(){
-        console.log(i);
- },i*1000);
-
+    for( var i = 1; i<= 5; i++){
+        function close(i){
+            setTimeout (function(){
+                console.log(i);
+         },i*1000);
+    
+    }
+    close(i);
+}
  console.log("Namaste JS")
-  
+ 
 }
 x();
