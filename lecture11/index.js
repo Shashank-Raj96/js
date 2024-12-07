99e95e69bfe6f7fb12578b767e7039479cf3d676
Shashank-Raj96 <shashankraj0915@gmail.com>
2024-12-08 00:47:00 +0530

    named function expression

---
+++ lecture11/index.js
@@ -0,0 +1,31 @@
+ 
+ // Function Statement aka Function Declaration
+function a(){
+    console.log("a called");
+}
+
+
+
+// function Expression
+ var b = function xyz(){
+     console.log(xyz);
+ }
+  a();
+  b();
+  xyz();
+
+// Anonymous Function
+// function (){
+
+// }
+
+// Named Function Expression
+// var b = function xyz(){
+//     console.log("b called");
+// }
+
+// xyz();
+
+
+
+
