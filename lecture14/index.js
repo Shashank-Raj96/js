9627c2b5f15d6af30635ac4c6d0bf69eb1bb523a
Shashank-Raj96 <shashankraj0915@gmail.com>
2024-12-23 17:03:03 +0530

    calculating area , circumference , diameter

--- lecture14/index.js
+++ lecture14/index.js
@@ -9,3 +9,23 @@ const calculateArea = function (radius){
     return output;
 }
 console.log(calculateArea(radius));
+
+const calculateCircumference = function (radius){
+    const output = [];
+    for(let i =0; i<radius.length ; i++){
+        output.push(2 * Math.Pi *radius[i] );
+
+    }
+    return output;
+}
+console.log(calculateCircumference(radius));
+
+const calculateDiameter = function (radius){
+    const output = [];
+    for(let i =0; i<radius.length ; i++){
+        output.push(2 * radius[i] );
+
+    }
+    return output;
+}
+console.log(calculateDiameter(radius));
