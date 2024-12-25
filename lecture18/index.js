ddd33c45f1ab4c4c18e1af709f621a465cb0f60e
Shashank-Raj96 <shashankraj0915@gmail.com>
2024-12-26 02:09:22 +0530

    Promise.allsettled API

--- lecture18/index.js
+++ lecture18/index.js
@@ -4,13 +4,14 @@
  })
 
  const p2 = new Promise((resolve, reject)=>{
-    setTimeout(()=> resolve("p2 success"),1000);
+    //setTimeout(()=> resolve("p2 success"),1000);
+    setTimeout(()=> reject("p2 fail"),1000);
  })
 
  const p3 = new Promise((resolve, reject)=>{
     setTimeout(()=> resolve("p3 success"),2000);
  })
 
- Promise.all([p1,p2,p3]).then(result=>{
+ Promise.allSettled([p1,p2,p3]).then(result=>{
     console.log(result);
  });
