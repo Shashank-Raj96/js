96d46a225d6fe9b01c23e0ae33fcf18f8de4f1e6
Shashank-Raj96 <shashankraj0915@gmail.com>
2024-12-25 16:05:44 +0530

    promise using fetch

--- lecture16/index.js
+++ lecture16/index.js
@@ -1,13 +1,21 @@
-const cart = ("shoes" , "pants" , "kurtas");
+// const cart = ("shoes" , "pants" , "kurtas");
 
-createOrder(cart , function(){
-    proceedToPayment(orderId);
-});//orderId
+// createOrder(cart , function(){
+//     proceedToPayment(orderId);
+// });//orderId
 
-const promise = createOrder(cart);
+// const promise = createOrder(cart);
 
-// promise = (data : undefined {empty})
+// // promise = (data : undefined {empty})
 
-promise.then(function(){
-    proceedToPayment(orderId);
-});//orderId);
+// promise.then(function(){
+//     proceedToPayment(orderId);
+// });//orderId);
+
+// to make an API call to github server and we will get the user info
+
+const GITHUB_API = "https://github.com/Shashank-Raj96"
+
+ const user = fetch(GITHUB_API);
+
+ console.log(user);
