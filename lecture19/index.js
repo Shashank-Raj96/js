6f844352c767252c42f74f62f6be7d697f34470f
Shashank-Raj96 <shashankraj0915@gmail.com>
2024-12-26 20:48:54 +0530

    async await when there are two different promises

--- lecture19/index.js
+++ lecture19/index.js
@@ -1,20 +1,26 @@
 
-const p = new Promise((resolve, reject)=>{
+const p1 = new Promise((resolve, reject)=>{
     setTimeout (() => {
         resolve("Promise Resolved Value");
     },10000);
     
 });
 
+const p2 = new Promise((resolve, reject)=>{
+    setTimeout (() => {
+        resolve("Promise Resolved Value");
+    },5000);
+    
+});
+
   async function handlePromise(){
      //JS Engine was waiting for promise to resolve 
-      const val1 = await p;
+      const val1 = await p1;
       console.log ("Namaste JavaScript");
       console.log(val1);
-  }
-  handlePromise();
 
-  const val2 = await p;
+
+  const val2 = await p2;
   console.log ("Namaste JavaScript 2");
   console.log(val2);
 }
