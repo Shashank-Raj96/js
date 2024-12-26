e7e0cfc03eeec12828ca0b0413055dfe8ac3e035
Shashank-Raj96 <shashankraj0915@gmail.com>
2024-12-26 20:33:47 +0530

    Async Await program

--- lecture19/index.js
+++ lecture19/index.js
@@ -1,11 +1,29 @@
 
 const p = new Promise((resolve, reject)=>{
-    resolve("Promise Resolved Value");
+    setTimeout (() => {
+        resolve("Promise Resolved Value");
+    },10000);
+    
 });
-async function getData(){
-    return p;
 
-}
-const dataPromise = getData();
+  async function handlePromise(){
+     //JS Engine was waiting for promise to resolve 
+      const val = await p;
+      console.log ("Namaste JavaScript");
+      console.log(val);
+  }
+  handlePromise();
 
-dataPromise.then((res)=> console.log(res));
+// function getData(){
+//     //JS engine will not wait for promiose to resolve and it will print JS
+//     p.then((res)=>console.log(res));
+//     console.log("Namaste JavaScript");
+// }
+// getData();
+// async function getData(){
+//     return p;
+
+// }
+// const dataPromise = getData();
+
+// dataPromise.then((res)=> console.log(res));
